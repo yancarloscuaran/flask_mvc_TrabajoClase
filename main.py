4b1552b7449ec160bf06952937e9676f6e4c1793
yancarloscuaran <yancarloscuaranimbacuan@gmail.com>
2021-02-25 22:43:06 -0500

    Actualizando

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from src import create_app
 if __name__ == '__main__':
     create_app()
 
+#Funcionalidad agregar  
+# productos Nombre, descripcion, precio venta, precio compra, estado => Activo o Inactivo Repositorio en GITHUB Grupos de 2
